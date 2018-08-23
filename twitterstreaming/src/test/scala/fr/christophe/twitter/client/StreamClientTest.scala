01bf8d58c04e053b3ffcc0604d8241b5b4d42bcf
cfouxy <42417030+cfouxy@users.noreply.github.com>
2018-08-23 18:58:39 +0200

    Update StreamClientTest.scala

--- twitterstreaming/src/test/scala/fr/christophe/twitter/client/StreamClientTest.scala
+++ twitterstreaming/src/test/scala/fr/christophe/twitter/client/StreamClientTest.scala
@@ -1,14 +1,32 @@
 package fr.christophe.twitter.client
 
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
 
-class StreamClientTest extends  FlatSpec with Matchers {
+class StreamClientTest extends FlatSpec with Matchers {
 
 
-  "http://" should "be matched by regex" in {
-    val Pattern = ".*(http|https)://(.*)".r
+  "http" should  "be matched by regex" in {
+
+    val Pattern = """.*(http|https):\/\/.*""".r
 
     "http://" match {
+      case Pattern(matched) => println(s"HTTP : ${matched}")
+      case a => println(s"NO HTTP : $a")
+    }
+
+    "https://" match {
+      case Pattern(matched) => println(s"HTTP : ${matched}")
+      case a => println(s"NO HTTP : $a")
+    }
+
+    "bla bla https://www.bla.com bla bla " match {
+      case Pattern(matched) => println(s"HTTP : ${matched}")
+      case a => println(s"NO HTTP : $a")
+    }
+
+
+    "https" match {
       case Pattern(matched) => println(s"HTTP : ${matched}")
       case a => println(s"NO HTTP : $a")
     }
