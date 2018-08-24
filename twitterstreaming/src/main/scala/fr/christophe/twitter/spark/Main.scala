a5288b1381b021e92b562a979650489cf122a20a
cfouxy <42417030+cfouxy@users.noreply.github.com>
2018-08-24 17:13:48 +0200

    Create Main.scala

---
+++ twitterstreaming/src/main/scala/fr/christophe/twitter/spark/Main.scala
@@ -0,0 +1,32 @@
+package fr.christophe.twitter.spark
+
+import fr.christophe.twitter.Decision
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.twitter._
+import org.apache.spark.SparkConf
+
+object Main extends App {
+
+  System.setProperty("twitter4j.oauth.consumerKey", "9do1BHyk2C6MEpq4z5D7Cp7IG")
+  System.setProperty("twitter4j.oauth.consumerSecret", "iNAZIRF1xmjGinUfFMc8EhHohZmXCtttFckYm5mXpYJqdBXEYu")
+  System.setProperty("twitter4j.oauth.accessToken", "819910726982791169-MB4yQz959ayVMXRXlIHGtlRQCT0LEe5")
+  System.setProperty("twitter4j.oauth.accessTokenSecret", "2AYOkNIWoMLMrbClJfUohUYijlXnlTMo9AEQVM2WBerYY")
+
+  implicit val sparkConf = new SparkConf().setMaster("local[*]").setAppName("TwitterLiveStreaming")
+  implicit val ssc = new StreamingContext(sparkConf, Seconds(2))
+
+  startStreaming
+
+  def startStreaming() (implicit ssc: StreamingContext) = {
+
+    val stream = TwitterUtils.createStream(ssc, None)
+
+    stream.foreachRDD(
+      statusRDD =>
+        statusRDD.foreach(
+          status => Decision.getApplication(status).run()))
+
+  }
+
+}
+
