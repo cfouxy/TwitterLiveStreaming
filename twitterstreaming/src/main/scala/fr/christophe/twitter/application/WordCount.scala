16d3ad79b5996657cddd2d75c0487b05f01ad736
cfouxy <42417030+cfouxy@users.noreply.github.com>
2018-08-27 18:25:21 +0200

    Update WordCount.scala

--- twitterstreaming/src/main/scala/fr/christophe/twitter/application/WordCount.scala
+++ twitterstreaming/src/main/scala/fr/christophe/twitter/application/WordCount.scala
@@ -1,12 +1,28 @@
 package fr.christophe.twitter.application
+
+import fr.christophe.twitter.dao.WordsCountDao._
 import org.apache.spark.rdd.RDD
 import twitter4j.Status
 
 class WordCount extends Application {
 
   override def run(statusRDD: RDD[Status]): Unit = {
-    super.run(statusRDD)
-    statusRDD.map(status =>
-    status.getText.split("""\\s+"""))
+    val counters = count(statusRDD)
+    save(counters)
+  }
+
+  private def count(statusRDD: RDD[Status]): RDD[(String, Int)] = {
+    statusRDD.flatMap(status =>
+      status.getText.split("""\\s+"""))
+      .map(word => (word, 1))
+      .reduceByKey(_ + _)
+  }
+
+  private def save(counters: RDD[(String, Int)]): Unit = {
+    counters.foreach(tuple => {
+      val wordCounterFound = getCount(tuple._1)
+      val wordCounterNew = wordCounterFound.copy(counter = wordCounterFound.counter + tuple._2)
+      updateCount(wordCounterNew)
+    })
   }
 }
