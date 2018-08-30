f25dcd7ac7becc729dc8af48fe4774f2f81ed9e7
cfouxy <42417030+cfouxy@users.noreply.github.com>
2018-08-30 10:19:34 +0200

    Update WordCountTest.scala

--- twitterstreaming/src/test/scala/fr/christophe/twitter/application/WordCountTest.scala
+++ twitterstreaming/src/test/scala/fr/christophe/twitter/application/WordCountTest.scala
@@ -2,18 +2,8 @@ package fr.christophe.twitter.application
 
 import com.holdenkarau.spark.testing.StreamingSuiteBase
 import org.scalatest.{FlatSpec, Matchers, WordSpec}
-//import org.apache.spark.
+
 
 class WordCountTest extends FlatSpec with StreamingSuiteBase with Matchers {
 
-  "aze" should "aze" in {
-
-
-    "azr"
-  }
-
-
-  override def convertToLegacyEqualizer[T](left: T) = ???
-
-  override def convertToLegacyCheckingEqualizer[T](left: T) = ???
 }
