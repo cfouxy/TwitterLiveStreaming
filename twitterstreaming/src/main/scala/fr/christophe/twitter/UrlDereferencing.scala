e29bf1f14960df923f15c33a8b4c1954055bc1e9
cfouxy <42417030+cfouxy@users.noreply.github.com>
2018-08-30 14:23:35 +0200

    Delete UrlDereferencing.scala

--- twitterstreaming/src/main/scala/fr/christophe/twitter/UrlDereferencing.scala
+++
@@ -1,9 +0,0 @@
-package fr.christophe.twitter.application
-import org.apache.spark.rdd.RDD
-import twitter4j.Status
-
-class UrlDereferencing extends Application {
-
-  override def run(statusRDD: RDD[Status]): Unit = ???
-
-}
